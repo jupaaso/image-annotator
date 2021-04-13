6d91f25e52ba4de2f457991887f74e230c05cc26
MeriaHanna4AI <55892486+MeriaHanna@users.noreply.github.com>
2021-04-13 21:38:36 +0300

    Update db_test.py
    Updated db test file, it is running now

--- tests/db_test.py
+++ tests/db_test.py
@@ -7,6 +7,15 @@
 # same as above but with console output
 # (.venv) C:\PWBproject\ImageAnnotator\tests>python -m pytest -s 
 
+# import python packages -----------------------------------
+
+import json
+import time
+from jsonschema import validate
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+from sqlalchemy.exc import IntegrityError, StatementError
+
 import os
 import pytest
 import tempfile
@@ -14,15 +23,27 @@ from datetime import datetime
 import base64
 from io import BytesIO
 
+# import hub (image annotator) ------------------------------
+
 import hub
 from hub import create_app, db
-from hub.models import User, ImageAnnotation, ImageContent, PhotoAnnotation
+from hub.models import User, ImageContent, ImageAnnotation, PhotoAnnotation
+
+#import config
+#from config import config
+
+# -----------------------------------------------------------
+# creates temporary database only for testing purposes
 
 @pytest.fixture
 def app():
     db_fd, db_fname = tempfile.mkstemp()    
     
-    app = create_app("testing")
+    config = {
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///" + db_fname,
+        "TESTING": True
+    }
+    app = create_app(config)
     
     with app.app_context():
         db.create_all()
@@ -31,36 +52,26 @@ def app():
     os.close(db_fd)
     os.unlink(db_fname)
 
+# -----------------------------------------------------------
 
 def _get_image():
     location = "C:\\PWBproject\\ImageAnnotator\\tests\\"
-    name = 'kuha meemi1.jpg'
-    with open(location + name, "rb") as f:
+    imagefilename = 'kuha meemi1.jpg'
+    with open(location + imagefilename, "rb") as f:
         image_binary = f.read()
         image_ascii = base64.b64encode(image_binary).decode('ascii')
+        timestamp = os.path.getctime(imagefilename)
+        datetime_str = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+        
         image_dict = {
-            "image_data":image_binary,
-            "image_ascii":image_ascii,
-            "name":name,
-            "publish_date":publish_date,
-            "location":location
+            "image_data": image_binary,
+            "image_ascii": image_ascii,
+            "name": imagefilename,
+            "publish_date": datetime.fromisoformat(datetime_str),
+            "location": location,
+            "date": datetime.fromisoformat(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         }
         return image_dict
-
-def _get_photo():
-    location = 'C:\\PWBproject\\ImageAnnotator\\tests\\'
-    name = 'Norja 2020.jpg'
-    with open(location + name, "rb") as f:
-        image_binary = f.read()
-        image_ascii = base64.b64encode(image_binary).decode('ascii')
-        image_dict = {
-            "image_data":image_binary,
-            "image_ascii":image_ascii,
-            "name":name,
-            "publish_date":publish_date,
-            "location":location
-        }
-        return photo_dict
 
 def _get_user():
     user = User(user_name = "testaaja", user_password="mfir7ihf9w8")
@@ -74,7 +85,7 @@ def _get_image_content(received_is_private):
         name=test_image["name"],
         publish_date=test_image["publish_date"],
         location=test_image["location"],
-        is_private=received_is_private
+        is_private=received_is_private,
         date=datetime.now()
     )
 
@@ -91,27 +102,17 @@ def _get_image_annotation():
         text_language="Finnish"
     )
 
-def _get_photo_content():
-    test_photo = _get_photo()
-    return PhotoContent(
-        data=test_photo["image_data"],
-        ascii_data=test_photo["image_ascii"],
-        name=test_photo["name"],
-        publish_date=test_photo["publish_date"],
-        location=test_photo["location"],
-        is_private=received_is_not_private
-        date=datetime.now()
-    )
-
 def _get_photo_annotation():
     return PhotoAnnotation(
         persons_class = False,
         slideshow_class = False,
         positivity_class = 2,
-        text_free_comment = "Beach",
+        text_free_comment = "Beach is beautiful",
         text_persons = "Text here",
         text_persons_comment = "Comment here",
     )
+
+# --------------------------------------------------------------
 
 def test_create_model_instances(app):
     """
@@ -123,46 +124,46 @@ def test_create_model_instances(app):
     
     with app.app_context():
         # Create everything
+        
+        # add user
         user = _get_user()
         db.session.add(user)
         db.session.commit()
-        #
+        # query user from database
         newUser = User.query.filter_by(id=1).first()
+        
+        # add image for user
         image = _get_image_content(False)
         newUser.image_user.append(image)
         db.session.commit()
-        #
+        # add annotation for user and image
         newImage = ImageContent.query.filter_by(id=1).first()
         image_annotation = _get_image_annotation()
         newUser.image_annotator.append(image_annotation)
         newImage.image_annotations.append(image_annotation)
         db.session.commit()
-        #
+        
+        # add photo for user
         photo = _get_image_content(True)
-        #newUser.photo_user.append(photo)
-        newUser.image_user.append(photo)
+        newUser.photo_user.append(photo)
         db.session.commit()
-
-        #newPhoto = PhotoContent.query.filter_by(id=1).first()
+        # add annotation for user and photo
         newPhoto = ImageContent.query.filter_by(id=2).first()
-
         photo_annotation = _get_photo_annotation()
         newUser.photo_annotator.append(photo_annotation)
         newPhoto.photo_annotations.append(photo_annotation)
         db.session.commit()
 
+        
         # Check that everything exists --------------------
         assert User.query.count() == 1
-        assert ImageContent.query.count() == 1
-        assert ImageAnnotation.query.count() == 1
-        assert PhotoContent.query.count() == 1
+        assert ImageContent.query.count() == 2
+        assert ImageAnnotation.query.count() == 1        
         assert PhotoAnnotation.query.count() == 1
 
         db_user = User.query.first()
         db_imagecontent = ImageContent.query.first()
         db_imageannotation = ImageAnnotation.query.first()
-        #db_photocontent = PhotoContent.query.first()
-        db_photocontent = ImageContent.query.first()
         db_photoannotation = PhotoAnnotation.query.first()
 
         # Check all relationships --------------------------
@@ -177,4 +178,3 @@ def test_create_model_instances(app):
         assert db_imageannotation.image[0] == db_imagecontent
         
         # TEE MYÖS LOPUT VERTAILUT kaikille suhteille
-        
